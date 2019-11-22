24dc8ec8ccfd5b667c27605381174f54f987b919
HIMAXCN\610323 <morgen_cai@himax.com.cn>
2019-11-22 13:44:40 +0800

    add initial files

---
+++ yunsheng/pages/health_science/health_science.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		health_science
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ yunsheng/pages/home/home.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		home
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
