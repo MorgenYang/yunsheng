c470468445446109ae57546ab47b968cd3d3b068
HIMAXCN\610323 <morgen_cai@himax.com.cn>
2019-11-22 15:23:48 +0800

    adjust content struct

---
+++ yunsheng/pages/home/sub_page/data_input/data_input.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		data input
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
