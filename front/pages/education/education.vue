fcf38c0e76ff1d65d9d446527817b89fe736e284
caiyang <cyxhxb@163.com>
2019-12-10 22:45:31 +0800

    Modify main modules

---
+++ front/pages/education/education.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		{{title}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'education'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ front/pages/me/me.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		{{title}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'me'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
