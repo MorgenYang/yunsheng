acb8a0a6345447d82286e42dbbb2eee0001f5f3e
huanghaobj <huanghaobj@126.com>
2019-12-14 21:05:23 +0800

    个人中心页面

--- front/pages/me/me.vue
+++ front/pages/me/me.vue
@@ -1,41 +1,33 @@
 <template>
-	<view class="page">
+	<view class="content">
+		<!-- #ifdef APP-PLUS -->
+		<view class="titleNview-placing"></view>
+		<!-- #endif -->
 		<view class="header">
-			demo
-			
-		</view>
-		<view>
-			<view>个人信息</view>
-			<scroll-view class="scroll-view-x" scroll-x="true">
-				<view class="scroll-view-item-x red">red</view>
-				<view class="scroll-view-item-x green">green</view>
-				<view class="scroll-view-item-x blue">blue</view>
-			</scroll-view>
+			<image class="user-img" :src="user.userImg"></image>
+			<view class="user-info">
+				<text class="user-name">{{user.userName}}</text>
+				<view class="user-other">
+					<text class="user-gender">{{user.userGender}}</text>
+					<text class="user-age">{{user.userAge}}岁</text>
+				</view>
+			</view>
+			<view class="user-load">
+				<image class="arrows-right-user" src="../../static/icon/settings/arrows-right-user.png"></image>
+			</view>
 		</view>
 		
-		<view>
-			<view>设置</view>
-			<view>
-				<image class="personal-settings" src="../../static/icon/settings/settings.png"></image>
-				<text>个人设置</text>
-			</view>
-			<view>
-				<image class="personal-settings" src="../../static/icon/settings/feedback.png"></image>
-				<text>意见反馈</text>
-			</view>
-			<view>
-				<image class="personal-settings" src="../../static/icon/settings/update.png"></image>
-				<text>检查更新</text>
-			</view>
-			<view>
-				<image class="personal-settings" src="../../static/icon/settings/us.png"></image>
-				<text>关于我们</text>
-			</view>
-			<view>
-				<image class="personal-settings" src="../../static/icon/settings/connection.png"></image>
-				<text>联系我们</text>
+		<view class="setting-body">
+			<view v-for="(item,index) in setting" v-bind:key="index.id">
+				<view class="setting-line" >
+					<image class="setting-img" :src="item.imageSrc"></image>
+					<text class="setting-text">{{item.title}}</text>
+					<image class="arrows-right-setting" src="../../static/icon/settings/arrows-right-setting.png"></image>
+				</view>
+				<view :class="{line:(index !=setting.length-1)}"></view>
 			</view>
 		</view>
+		
 	</view>
 </template>
 
@@ -43,7 +35,47 @@
 	export default {
 		data() {
 			return {
-				title: 'me'
+				title: 'me',
+				user:{
+					userName:"张伟",
+					userGender: "男",
+					userAge: 18,
+					userImg:"http://img0.imgtn.bdimg.com/it/u=1787676231,2721036559&fm=26&gp=0.jpg"
+				},
+				setting:[
+					{
+						title:"健康档案",
+						imageSrc:"../../static/icon/settings/health-record.png"
+					},
+					{
+						title:"健康商城",
+						imageSrc:"../../static/icon/settings/store.png"
+					},
+					{
+						title:"私人医生",
+						imageSrc:"../../static/icon/settings/doctor.png"
+					},
+					{
+						title:"个人设置",
+						imageSrc:"../../static/icon/settings/feedback.png"
+					},
+					{
+						title:"意见反馈",
+						imageSrc:"../../static/icon/settings/health-record.png"
+					},
+					{
+						title:"检查更新",
+						imageSrc:"../../static/icon/settings/update.png"
+					},
+					{
+						title:"关于我们",
+						imageSrc:"../../static/icon/settings/us.png"
+					},
+					{
+						title:"联系我们",
+						imageSrc:"../../static/icon/settings/connection.png"
+					}					
+				]	
 			}
 		},
 		onLoad() {
