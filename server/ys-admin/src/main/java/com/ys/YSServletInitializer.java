83dab2f9e60a206f89365ea7b5103ffac3f9ad1e
youf <youfan1989@sina.com>
2019-12-22 22:49:57 +0800

    删除target文件夹

---
+++ server/ys-admin/src/main/java/com/ys/YSServletInitializer.java
@@ -0,0 +1,16 @@
+package com.ys;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * web容器中进行部署
+ */
+public class YSServletInitializer extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(YSApplication.class);
+    }
+}
