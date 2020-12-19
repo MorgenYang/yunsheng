8bc6aa81c331eb1bdbde2fe1cd6393a9d99bb0be
caiyang <cyxhxb@163.com>
2020-12-19 13:47:44 +0800

    修正登录注册和获取手机号逻辑

--- front/pages/me/me.vue
+++ front/pages/me/me.vue
@@ -1,27 +1,20 @@
 <template>
 	<view class="content">
-		<view class="header" v-if="(user.phone != '' && user.phone != null) && user.openid != ''">
+		<view class="header" v-if="user.openid != ''">
 			<image class="user-img" :src="user.avatarUrl"></image>
 			<view class="user-info">
 				<text class="user-name">{{user.nickName}}</text>
 				<view class="user-other">
-					<!-- <text class="user-gender" v-if="user.gender=='1'">男</text>
-					<text class="user-gender" v-else-if="user.gender=='0'">女</text>
-					<text class="user-gender" v-else></text> -->
-					<text class="user-age">手机号：{{user.phone}}</text>
+					<button class="user-phone" @getphonenumber="getPhoneNumber" open-type="getPhoneNumber" v-if="user.phone == '' || user.phone == null">请绑定手机号</button>
+					<text class="user-age" v-else>手机号：{{user.phone}}</text>
 				</view>
 			</view>
-			<view class="user-load">
-				<image class="arrows-right-user" src="../../static/icon/settings/arrows-right-user.png"></image>
-			</view>
-		</view>
-		<view class="header-for-login" v-else-if="user.openid != '' && (user.phone == '' || user.phone == null)">
-			<button class="header-for-login-btn" @getphonenumber="getPhoneNumber" open-type="getPhoneNumber">获取手机号</button>
 		</view>
 		<view class="header-for-login" v-else>
-			<button class="header-for-login-btn" @getuserinfo="getUserInfo" open-type="getUserInfo">微信登录</button>
+			<button class="header-for-login-btn" @getuserinfo="getUserInfo" open-type="getUserInfo">登录注册</button>
 		</view>
 
+		<!-- body -->
 		<view class="setting-body">
 			<view v-for="(item,index) in setting" hover-class="setting-click" v-bind:key="item.id" @tap="itemClick" v-bind:id="'id_'+item.id">
 				<view class="setting-line">
@@ -77,20 +70,20 @@
 						imageSrc: "../../static/icon/settings/feedback.png",
 						linkUrl: "./feedback/feedback"
 					},
+					// {
+					// 	id: 5,
+					// 	title: "检查更新",
+					// 	imageSrc: "../../static/icon/settings/update.png",
+					// 	linkUrl: ""
+					// },
 					{
 						id: 5,
-						title: "检查更新",
-						imageSrc: "../../static/icon/settings/update.png",
-						linkUrl: ""
-					},
-					{
-						id: 6,
 						title: "关于我们",
 						imageSrc: "../../static/icon/settings/us.png",
 						linkUrl: "./about/about"
 					},
 					{
-						id: 7,
+						id: 6,
 						title: "联系我们",
 						imageSrc: "../../static/icon/settings/connection.png",
 						linkUrl: "./contact/contact"
@@ -107,7 +100,8 @@
 
 			console.log(this.user.phone);
 			console.log(this.user.openid);
-			console.log(this.user.phone);
+			console.log(this.user.avatarUrl);
+			
 		},
 		methods: {
 			getUserInfo(e) {
@@ -124,10 +118,9 @@
 						uni.login({
 							provider: 'weixin',
 							success: (res) => {
-								console.log(res.code);
 								//这里需要做登录验证操作，如果用户存在直接登录，不存在就注册
 								uni.request({
-									url: that.domainName + '/wechatlogin',
+									url: that.reqAddress + '/wechatlogin',
 									method: "POST",
 									data: {
 										"code": res.code
@@ -138,9 +131,8 @@
 											that.user.openid = res.data.data.loginClientUserVo.openid;
 											that.user.phone = res.data.data.loginClientUserVo.phone;
 											uni.setStorageSync('openid', that.user.openid);
-											uni.setStorageSync('phone', that.user.phone);
 											uni.showToast({
-												title: '登录成功',
+												title: '授权成功',
 												duration: 2000
 											});
 										} else {
@@ -148,9 +140,8 @@
 											uni.login({
 												provider: 'weixin',
 												success: (res) => {
-													console.log(res.code);
 													uni.request({
-														url: that.domainName + '/wechatRegister',
+														url: that.reqAddress + '/wechatRegister',
 														method: "POST",
 														data: {
 															"avatarUrl": that.user.avatarUrl,
@@ -162,7 +153,6 @@
 															"province": ""
 														},
 														success: (res) => {
-															console.log(res.data);
 															if (200 == res.data.code) {
 																console.log(res.data.data.openid);
 																that.user.openid = res.data.data.openid;
@@ -213,7 +203,7 @@
 					provider: 'weixin',
 					success: function(loginRes) {
 						uni.request({
-							url: that.domainName + '/wechatPhone',
+							url: that.reqAddress + '/wechatPhone',
 							method: "POST",
 							data: {
 								"code": loginRes.code,
